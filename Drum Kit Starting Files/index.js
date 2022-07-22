f1e27c38f8f9c4b83aae2c62040edb6eda71090b
DozzyMontalo <dozienice@gmail.com>
2022-07-22 02:38:46 +0100

    2nd commit

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,15 +1,3 @@
-// document.querySelector("button").addEventListener("click", handleClick)
-
-// function handleClick() {
-//     alert ("I got clicked")
-
-// }
-
-
-// document.querySelector("button").addEventListener("click",function(){
-//     alert("I got clicked")
-// })
-
 //Detecting Button Press
 for(var i = 0; i < document.querySelectorAll(".drum").length; i++ ){
   document.querySelectorAll(".drum")[i].addEventListener("click",function(){
